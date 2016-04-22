73d15264bcca1cecfe91eae9216ca7ea1c406152
elex <elextraza@gmail.com>
2016-04-22 11:34:33 +0300

    Красивый шаблон

--- application/views/nf_footer.php
+++ application/views/nf_footer.php
@@ -16,66 +16,66 @@
 <!--=================================================-->
 
 <!--jQuery [ REQUIRED ]-->
-<script src="/js/nf/jquery-2.1.1.min.js"></script>
+<script src="<?php echo site_url('js/nf/jquery-2.1.1.min.js');?>"></script>
 
 
 <!--BootstrapJS [ RECOMMENDED ]-->
-<script src="/js/nf/bootstrap.min.js"></script>
+<script src="<?php echo site_url('js/nf/bootstrap.min.js');?>"></script>
 
 
 <!--Fast Click [ OPTIONAL ]-->
-<script src="/js/nf/fastclick.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>fastclick.min.js"></script>
 
 
 <!--Nifty Admin [ RECOMMENDED ]-->
-<script src="/js/nf/nifty.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>nifty.min.js"></script>
 
 
 <!--Switchery [ OPTIONAL ]-->
-<script src="/js/nf/switchery.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>switchery.min.js"></script>
 
 
 <!--Bootstrap Select [ OPTIONAL ]-->
-<script src="/js/nf/bootstrap-select.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>bootstrap-select.min.js"></script>
 
 
 <!--Bootstrap Tags Input [ OPTIONAL ]-->
-<script src="/js/nf/bootstrap-tagsinput.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>bootstrap-tagsinput.min.js"></script>
 
 
 <!--Chosen [ OPTIONAL ]-->
-<script src="/js/nf/chosen.jquery.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>chosen.jquery.min.js"></script>
 
 
 <!--noUiSlider [ OPTIONAL ]-
-<script src="/js/nf/jquery.nouislider.all.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>jquery.nouislider.all.min.js"></script>
 -->
 
 <!--Bootstrap Timepicker [ OPTIONAL ]-->
-<script src="/js/nf/bootstrap-timepicker.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>bootstrap-timepicker.min.js"></script>
 
 
 <!--Bootstrap Datepicker [ OPTIONAL ]-->
-<script src="/js/nf/bootstrap-datepicker.js"></script>
+<script src="<?php echo site_url('js/nf/');?>bootstrap-datepicker.js"></script>
 
 
 <!--Dropzone [ OPTIONAL ]-->
-<script src="/js/nf/dropzone.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>dropzone.min.js"></script>
 
 
 <!--Summernote [ OPTIONAL ]-->
-<script src="/js/nf/summernote.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>summernote.min.js"></script>
 
 
 <!--Demo script [ DEMONSTRATION ]-->
-<script src="/js/nf/nifty-demo.min.js"></script>
+<script src="<?php echo site_url('js/nf/');?>nifty-demo.min.js"></script>
 
 
 <!--Form Component [ SAMPLE ]
-<script src="/js/nf/form-component.js"></script>
+<script src="<?php echo site_url('js/nf/');?>form-component.js"></script>
 -->
-<script type="text/javascript" src="/js/jquery.datetimepicker.js"></script>
-<script src="/js/main.js"></script>
+<script type="text/javascript" src="<?php echo site_url('js/');?>jquery.datetimepicker.js"></script>
+<script src="<?php echo site_url('js/');?>main.js"></script>
 
 
 
